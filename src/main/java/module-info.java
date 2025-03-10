5523598f7e30a03a694d96842461f71eedbff6ac
mrcdslpz <163123003+mrcdslpz@users.noreply.github.com>
2025-03-10 13:14:54 +0100

    FEAT: Initialize JavaFX application with basic structure and Maven wrapper

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.pds.curiousmind {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.pds.curiousmind to javafx.fxml;
+    exports com.pds.curiousmind;
+}
