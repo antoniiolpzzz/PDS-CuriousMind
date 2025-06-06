3ae398d4605b4edfeddcd3d6feadcfa2623a0ec8
Antonio López Toboso <160279528+antoniiolpzzz@users.noreply.github.com>
2025-06-06 10:53:37 +0200

    FEAT: Simplify RegisteredContentBlock by removing progress and lives attributes

--- src/main/java/com/pds/curiousmind/model/registeredContentBlock/RegisteredContentBlock.java
+++ src/main/java/com/pds/curiousmind/model/registeredContentBlock/RegisteredContentBlock.java
@@ -11,19 +11,14 @@ public class RegisteredContentBlock {
     private String name;
     private List<Question> questions;
     private Difficulty difficulty;
-    private int progress;
     private boolean isCompleted;
-    private int lives; // Default lives
-
 
 
     public RegisteredContentBlock(ContentBlock contentBlock) {
         this.name = contentBlock.name;
         this.questions = contentBlock.questions;
         this.difficulty = contentBlock.difficultyLevel;
-        this.progress = 0; // Default progress
         this.isCompleted = false; // Default completion status
-        this.lives = 5; // Default lives
     }
 
     public String getName() {
@@ -38,34 +33,19 @@ public class RegisteredContentBlock {
         return difficulty;
     }
 
-    public int getProgress() {
-        return progress;
-    }
-
     public boolean isCompleted() {
         return isCompleted;
     }
 
-    public int getLives() {
-        return lives;
-    }
-
     // METHODS
-
-    public void updateProgress() {
-        this.progress = getCompletedQuestionsCount()/questions.size() * 100;
-    }
 
     public void markAsCompleted(Boolean isCorrect) {
             this.isCompleted = true;
-            updateProgress();
-
     }
 
-    public void quitCOntentBlock() {
+    public void quitContentBlock() {
         this.isCompleted = false;
-        this.progress = 0; // Reset progress
-        this.lives = 5; // Reset lives
+
     }
 
 
