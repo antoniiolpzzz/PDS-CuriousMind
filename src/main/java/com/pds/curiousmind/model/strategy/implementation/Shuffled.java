66a12f84baf1c340d4a8cbe1e871967610672b96
Antonio López Toboso <160279528+antoniiolpzzz@users.noreply.github.com>
2025-06-09 18:20:07 +0200

    FEAT: Update getQuestionsBlock method to use ArrayList for question storage

--- src/main/java/com/pds/curiousmind/model/strategy/implementation/Shuffled.java
+++ src/main/java/com/pds/curiousmind/model/strategy/implementation/Shuffled.java
@@ -8,7 +8,7 @@ public class Shuffled implements Strategy {
 
     public List<Question> getQuestionsBlock(ContentBlock block){
 
-        List<Question> questions = new List<Question>(block.getQuestions());
+        List<Question> questions = new ArrayList<Question>(block.getQuestions());
         Collections.shuffle(questions);
         return questions;
     }
