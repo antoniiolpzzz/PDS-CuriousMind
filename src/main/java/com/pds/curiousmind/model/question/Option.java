39429697b0ff81be8e0f576b9f0a288f045320f6
Antonio López Toboso <160279528+antoniiolpzzz@users.noreply.github.com>
2025-06-09 20:57:03 +0200

    FEAT: Trim label in Option constructor and implement equals/hashCode methods

--- src/main/java/com/pds/curiousmind/model/question/Option.java
+++ src/main/java/com/pds/curiousmind/model/question/Option.java
@@ -1,14 +1,30 @@
 package com.pds.curiousmind.model.question;
 
+import java.util.Objects;
+
 public class Option {
     private String label;
 
     public Option(String label) {
-        this.label = label;
+        this.label = label.trim();
     }
 
     public String getLabel() {
         return label;
+    }
+
+    //COMMON OVERRIDES
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Option option = (Option) obj;
+        return Objects.equals(this.label, option.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.label);
     }
 }
 
