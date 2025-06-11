5ca4265c82c96df707dc451b98634431b93abc55
mrcdslpz <163123003+mrcdslpz@users.noreply.github.com>
2025-06-11 13:15:16 +0200

    FEAT: Implement login window with user authentication interface

---
+++ src/main/java/com/pds/curiousmind/view/authentication/login/LoginWindow.java
@@ -0,0 +1,99 @@
+package com.pds.curiousmind.view.authentication.login;
+
+import com.pds.curiousmind.view.authentication.signup.SignupWindow;
+import com.pds.curiousmind.view.authentication.login.components.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class LoginWindow extends JFrame {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JCheckBox showPasswordCheckBox;
+
+    public LoginWindow() {
+        setTitle("CuriousMind - Log in");
+        setMinimumSize(new Dimension(1200, 600));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(true);
+
+        BackgroundPanel basePanel = new BackgroundPanel("icons/A3B80D76-7857-4DF3-8E9B-77619D7A2D56_1_105_c.jpeg");
+        basePanel.setLayout(new BorderLayout());
+        setContentPane(basePanel);
+
+        JPanel rightWrapper = new JPanel(new BorderLayout());
+        rightWrapper.setOpaque(false);
+        rightWrapper.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 20));
+
+        RoundedPanel rightPanel = new RoundedPanel(30);
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        rightPanel.setBackground(Color.WHITE);
+        rightPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+        rightWrapper.add(rightPanel, BorderLayout.CENTER);
+
+        basePanel.add(rightWrapper, BorderLayout.EAST);
+        rightWrapper.setPreferredSize(new Dimension(370, 0));
+
+        JLabel loginTitle = new JLabel("Log in");
+        loginTitle.setFont(new Font("SansSerif", Font.BOLD, 24));
+        loginTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+        rightPanel.add(loginTitle);
+        rightPanel.add(Box.createVerticalStrut(25));
+
+        usernameField = new JTextField();
+        usernameField.setBorder(BorderFactory.createTitledBorder("Username or email"));
+        usernameField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));
+        rightPanel.add(usernameField);
+        rightPanel.add(Box.createVerticalStrut(20));
+
+        passwordField = new JPasswordField();
+        passwordField.setBorder(BorderFactory.createTitledBorder("Password"));
+        passwordField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));
+        rightPanel.add(passwordField);
+        rightPanel.add(Box.createVerticalStrut(10));
+
+        JPanel optionsPanel = new JPanel(new BorderLayout());
+        optionsPanel.setOpaque(false);
+        showPasswordCheckBox = new JCheckBox("Show password");
+        showPasswordCheckBox.setOpaque(false);
+        showPasswordCheckBox.setForeground(Color.GRAY);
+        showPasswordCheckBox.addActionListener((ActionEvent e) -> {
+            passwordField.setEchoChar(showPasswordCheckBox.isSelected() ? (char) 0 : '\u2022');
+        });
+        optionsPanel.add(showPasswordCheckBox, BorderLayout.WEST);
+        rightPanel.add(optionsPanel);
+        rightPanel.add(Box.createVerticalStrut(25));
+
+        StyledButton loginButton = new StyledButton("Log in", Color.BLACK, Color.WHITE);
+        loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        loginButton.addActionListener(e -> {
+            dispose();
+            new HomeWindow();
+        });
+        rightPanel.add(loginButton);
+
+        rightPanel.add(Box.createVerticalStrut(15));
+        JLabel orLabel = new JLabel("Or", SwingConstants.CENTER);
+        orLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        orLabel.setForeground(Color.GRAY);
+        rightPanel.add(orLabel);
+        rightPanel.add(Box.createVerticalStrut(15));
+
+        StyledButton signupButton = new StyledButton("Sign up", new Color(240, 240, 240), Color.BLACK);
+        signupButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        signupButton.addActionListener(e -> {
+            new SignupWindow();
+            dispose();
+        });
+        rightPanel.add(signupButton);
+
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(LoginWindow::new);
+    }
+}
