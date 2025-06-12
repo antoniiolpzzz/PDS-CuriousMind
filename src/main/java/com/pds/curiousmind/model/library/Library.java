0b5c33597a6302a09c1929c1de9ab84771585e39
Antonio López Toboso <160279528+antoniiolpzzz@users.noreply.github.com>
2025-06-12 15:29:27 +0200

    FEAT: Add Library interface for managing library items

---
+++ src/main/java/com/pds/curiousmind/model/library/Library.java
@@ -0,0 +1,18 @@
+package com.pds.curiousmind.model.library;
+
+import java.util.List;
+
+/**
+ * Interface for a generic library that manages objects of type T.
+ * @param <T> the type of objects managed by the library
+ * @apiNote  This interface defines methods to add, remove, retrieve all objects,
+ * and get an object by its ID.
+ */
+public interface Library<T> {
+
+    void add(T object);
+    void remove(T object);
+    List<T> getAll();
+    T getById(Long id);
+
+}
