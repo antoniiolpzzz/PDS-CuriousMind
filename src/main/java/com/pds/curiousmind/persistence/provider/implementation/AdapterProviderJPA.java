0c224244d694b9af60902c90f8650ca5755709e9
Antonio López Toboso <160279528+antoniiolpzzz@users.noreply.github.com>
2025-06-14 20:42:00 +0200

    FEAT: Implement JPA adapter provider and persistence configuration

---
+++ src/main/java/com/pds/curiousmind/persistence/provider/implementation/AdapterProviderJPA.java
@@ -0,0 +1,34 @@
+package com.pds.curiousmind.persistence.provider.implementation;
+
+import com.pds.curiousmind.persistence.adapter.implementation.CourseAdapterJPA;
+import com.pds.curiousmind.persistence.adapter.implementation.UserAdapterJPA;
+import com.pds.curiousmind.persistence.adapter.interfaces.ICourseAdapter;
+import com.pds.curiousmind.persistence.adapter.interfaces.IUserAdapter;
+import com.pds.curiousmind.persistence.provider.AdapterProvider;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
+public class AdapterProviderJPA extends AdapterProvider {
+
+    private static final String PERSISTENCE_UNIT_NAME = "curiousmindPU";
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    private AdapterProviderJPA() {
+        this.entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        CourseAdapterJPA.INSTANCE.setEntityManagerFactory(entityManagerFactory);
+        UserAdapterJPA.INSTANCE.setEntityManagerFactory(entityManagerFactory);
+    }
+
+
+    @Override
+    public ICourseAdapter getCourseAdapter() {
+        return CourseAdapterJPA.INSTANCE;
+    }
+
+    @Override
+    public IUserAdapter getUserAdapter() {
+        return UserAdapterJPA.INSTANCE;
+    }
+}
