4b19f445fbb5cb68324b83ad3005abf889d736a1
mrcdslpz <163123003+mrcdslpz@users.noreply.github.com>
2025-06-16 17:06:52 +0200

    FEAT: Add application constants for title, dimensions, labels, and icons in GlobalConstants

--- src/main/java/com/pds/curiousmind/view/common/GlobalConstants.java
+++ src/main/java/com/pds/curiousmind/view/common/GlobalConstants.java
@@ -3,6 +3,24 @@ package com.pds.curiousmind.view.common;
 public class GlobalConstants {
 
     public static final String BACKGROUND_IMAGE_PATH = "icons/background/background.jpg";
+    public static final String APP_TITLE = "CuriousMind";
+    public static final String LOGIN_WELCOME = "WELCOME BACK!";
+    public static final String SIGNUP_WELCOME = "WELCOME!";
+    public static final int WINDOW_WIDTH = 1300;
+    public static final int WINDOW_HEIGHT = 650;
+    public static final int POPUP_WIDTH = 480;
+    public static final int POPUP_HEIGHT = 320;
+    public static final String LOGIN_LABEL = "Log in";
+    public static final String SIGNUP_LABEL = "Sign up";
+    public static final String FONT_NAME = "SansSerif ";
+    public static final String ICON_ANGRY = "icons/pet/enfadado.png";
+    public static final String ICON_HAPPY = "icons/pet/feliz.png";
+    public static final String ICON_FAIL = "icons/pet/fallo.png";
+    public static final String ICON_COMPLETE = "icons/pet/completado.png";
+
+
+
+
 
 
 }
