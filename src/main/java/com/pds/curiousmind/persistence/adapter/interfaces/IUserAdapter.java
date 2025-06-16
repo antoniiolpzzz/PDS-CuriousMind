f154ea6bd581df0270989429adf7dfeac2672a3b
Antonio López Toboso <160279528+antoniiolpzzz@users.noreply.github.com>
2025-06-17 01:58:51 +0200

    FIX: Update IAdapter interface to return Optional for findById method

--- src/main/java/com/pds/curiousmind/persistence/adapter/interfaces/IUserAdapter.java
+++ src/main/java/com/pds/curiousmind/persistence/adapter/interfaces/IUserAdapter.java
@@ -1,10 +1,12 @@
 package com.pds.curiousmind.persistence.adapter.interfaces;
 
+import com.pds.curiousmind.model.question.option.Option;
 import com.pds.curiousmind.model.user.User;
 import com.pds.curiousmind.persistence.adapter.IAdapter;
 
+import java.util.Optional;
+
 public interface IUserAdapter extends IAdapter<User> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
-
